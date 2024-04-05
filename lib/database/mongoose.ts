1d40ba2b271f1b6c62a58371808f84beb929638d
programmer-ayesha <ayeshagraphical@gmail.com>
2024-04-05 17:39:46 +0500

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
     cached.promise =
         cached.promise ||
         mongoose.connect(MONGODB_URL, {
-            dbName: 'canva', bufferCommands: false
+            dbName: 'imaginify', bufferCommands: false
         })
 
     cached.conn = await cached.promise;
